cf348defc03922ae39d7e652fbad8a2d84f8c40d
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-01-29 09:57:22 +0200

    Crosslinker: add manual crosslinks
    Closes #10
    
    Can be updated as needed based, based either on just noticing them or via SEO driven approach

---
+++ crosslinker/internal/crosslinks/manual-overrides.go
@@ -0,0 +1,11 @@
+package crosslinks
+
+func getManualAliases() map[string]string {
+	// Slugs. Generally west norse as key, east norse as value.
+	return map[string]string{
+		"dvergr":    "dvargher",
+		"geirlaukr": "hvitloker",
+		"hvalr":     "hval",
+		"hvitskinn": "hvitskinande-2",
+	}
+}
