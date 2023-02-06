a8cc3551bc43f638cf7c507198d7e6ae41487d09
Sampo Silvennoinen <silvennoinen.sampo@gmail.com>
2023-02-06 19:09:15 +0200

    Add simple minifier

---
+++ minifier/minifier.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "stscoundrel / Sampo Silvennoinen"
+description   = "Minifies crosslink output for scandinavian-dictionary-crosslinker"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["minifier"]
+
+
+# Dependencies
+
+requires "nim >= 1.6.0"
---
+++ minifier/src/minifier.nim
@@ -0,0 +1,36 @@
+import std/json
+import std/enumerate
+import std/strutils
+
+var jsonNode = parseFile("../resources/crosslinks.json")
+
+
+proc minify_link(link: string): string =
+  return link
+    .replace("https://old-icelandic.vercel.app/word", "")
+    .replace("https://old-norwegian-dictionary.vercel.app/word", "")
+    .replace("https://old-swedish-dictionary.vercel.app/word", "")
+    .replace("https://cleasby-vigfusson-dictionary.vercel.app/word", "")
+
+
+proc minify_source(link: string): string =
+  return link
+    .replace("old-norwegian", "onr")
+    .replace("old-swedish", "os")
+    .replace("old-norse", "on")
+    .replace("old-icelandic", "oi")
+
+
+for slug, links in jsonNode:
+  # Minify content.
+  for index, link in enumerate(links):
+    jsonNode[slug][index]["a"] = newJString(minify_link(link["url"].getStr()))
+    jsonNode[slug][index]["b"] = newJString(minify_source(link["source"].getStr()))
+
+    # Drop old content keys.
+    jsonNode[slug][index].delete("url")
+    jsonNode[slug][index].delete("source")
+
+
+# Minify & save.
+writeFile("../resources/crosslinks.min.json", pretty(jsonNode))
